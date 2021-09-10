1c75b007c1d7b36a2604a044ffea86325ae586f6
Gus Ramírez <gustavo@coconutt.io>
2021-09-09 19:19:18 -0500

    first commit

---
+++ src/App.spec.ts
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils';
+import App from './App.vue';
+
+describe('App', () => {
+  it('should render h1', () => {
+    const wrapper = shallowMount(App);
+
+    expect(wrapper.find('h1').exists()).toBeTruthy();
+  });
+});
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+  }
+</style>
